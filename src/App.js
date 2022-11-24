a4d1c5f9cf204e1ae9989e4e770a8780fcd50113
zuhdi751 <ahmadzuhdi751@gmail.com>
2022-11-24 18:40:22 +0700

    windowWidth using useEffect and clean-up function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // we will modivy a variable based on the width of the window
 function App() {
-  
-  return (
-    <div >
-    </div>
-  );
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    // clean up (stoping update position o windows especially when window is still remain unchanged)
+
+    // Try! using return in other useEffect calback function
+    // this is known as clean up function
+    // clean up function runs before the component is removed from the UI to prevent memory leaks
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div>{windowWidth}</div>;
 }
 
 export default App;
