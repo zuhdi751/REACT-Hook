d72d8dcc1b19bcfa30c35f9b232fbd0953ebd562
zuhdi751 <ahmadzuhdi751@gmail.com>
2022-11-24 14:05:05 +0700

    basic usage of useState and how to use it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(4)
+
+  function decrementCount () {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount () {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div className="App-container">
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
 
 export default App;
 
+// Hook or useState must be run from a function, not in loop or conditional or even class. useState must be put in the top level of function, can not be nested
+// always called in the exact same order.
