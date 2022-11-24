d7e592baa47a7cc11acf95985b6aa99fbbc6d1de
zuhdi751 <ahmadzuhdi751@gmail.com>
2022-11-24 16:18:09 +0700

    how useEffect work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [resourceType, setResourceType] = useState("posts");
+
+  // how useEffect work is everytime we render this component/function, everything in useEffect will be run. This tend to be dangerous due to continuous render. we do't want to get this so we put [] after a function inside useEffect. when something inside the [], it means that this variable inside [] will triger useEffect to be run.
+  useEffect(()=> {
+    console.log('render')
+  }, [resourceType])
 
   return (
     <div className="App-container">
@@ -17,5 +22,3 @@ function App() {
 
 export default App;
 
-// Hook or useState must be run from a function, not in loop or conditional or even class. useState must be put in the top level of function, can not be nested
-// always called in the exact same order.
