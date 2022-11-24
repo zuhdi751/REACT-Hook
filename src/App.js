cae97a1c81c3e18624cb73e5ca08a77cc462abb1
zuhdi751 <ahmadzuhdi751@gmail.com>
2022-11-24 14:39:00 +0700

    multiple hook in the same time

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(4)
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue')
 
-  function decrementCount () {
-    setCount(prevCount => prevCount - 1)
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+    setTheme('red')
   }
 
-  function incrementCount () {
-    setCount(prevCount => prevCount + 1)
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+    setTheme('green')
   }
 
   return (
     <div className="App-container">
+      <div>React Hook</div>
       <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
+      <span>{count} {theme}</span>
       <button onClick={incrementCount}>+</button>
     </div>
   );
