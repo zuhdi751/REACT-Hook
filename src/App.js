b8c6167df09574c9885f20b8fe5e1d7ae17440c0
zuhdi751 <ahmadzuhdi751@gmail.com>
2022-11-24 17:35:23 +0700

    starting point of useEffect-more/modify a variable based on window

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
+// we will modivy a variable based on the width of the window
 function App() {
-  const [resourceType, setResourceType] = useState("posts");
-  // show the api into web by using items
-  const [items, setItems] = useState([])
-
-  // everytime we click a button, 'render' is in console, but if we click post button we can't get 'render' is consoled even though we clicked it again and again(more than once). this is strange. it used to be consoled 'render' everytime post button is clicked. I can show this by change the text
-
-  // this is because function inside the useEffect will be run if only resourceType is change. when we click post button twice, resourceType is not changed. so thats why.
-
-  // explore it further by removing resourceType inside []
-  // when [] is empty, those callback function('unmount') doesn't work because empty array never actually changes between different renders.
-
-  // practice useEffect by fetchin data from 'jsonplaceholder.typecode.com/posts/'
   
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [resourceType]);
-
   return (
-    <div className="App-container">
-      <div>
-        <button onClick={() => setResourceType("posts")}>Posts</button>
-        <button onClick={() => setResourceType("users")}>Users</button>
-        <button onClick={() => setResourceType("comments")}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-
-      <p>{items.map((item) => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}</p>
-
-
+    <div >
     </div>
   );
 }
